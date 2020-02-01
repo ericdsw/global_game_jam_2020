0d792f5d10bb5793f02f6ee2db64206a3bc895c7
JuanLu23 <juanluisfv23@gmail.com>
2020-02-01 12:19:08 -0500

    Add fix marriage base implamantation

---
+++ Screens/MiniGames/BrokenMarriage/BrokenMarriageMinigame.gd
@@ -0,0 +1,39 @@
+extends BaseMinigame
+
+onready var marriage_button_scene : PackedScene = load("res://Screens/Minigames/BrokenMarriage/Buttons/MarriageButton.tscn")
+var wrong_options : Array = ["HIT HER", "DIVORCE", "TAKE THE NINTENDO WII"]
+
+func start(difficulty := 1) -> void:
+	.start(difficulty)
+	_spawn_buttons(5)
+
+func _choose_random_button_position(_button : Button) -> void:
+	randomize()
+	var viewport : Vector2 = get_viewport_rect().size
+	var fix_marriage_button_size = _button.rect_size 
+	_button.rect_global_position = Vector2(
+		rand_range(-viewport.x / 2.0, viewport.x / 2.0 - fix_marriage_button_size.x), 
+		rand_range(-viewport.y / 2.0, viewport.y / 2.0 - fix_marriage_button_size.y))
+
+func _spawn_buttons(_amount : int = 2) -> void:
+	for i in range(0, _amount):
+		var _button : Button = marriage_button_scene.instance()
+		add_child(_button)
+		_choose_random_button_position(_button)
+		
+		if i == 0:
+			_button.connect("pressed", self, "_pressed_correct_button")
+			_button.text = "FIX MARRIAGE"
+		else:
+			_button.connect("pressed", self, "_pressed_wrong_button")
+			wrong_options.shuffle()
+			if i > wrong_options.size():
+				_button.text = wrong_options.back()
+			else:
+				_button.text = wrong_options[i - 1]
+
+func _pressed_correct_button() -> void:
+	on_success()
+
+func _pressed_wrong_button() -> void:
+	on_failure()
--- Screens/MiniGames/BrokenMarriageMinigame.gd
+++
@@ -1,8 +0,0 @@
-extends BaseMinigame
-
-var foo : int = 0
-var bar := 0.0
-
-func start(difficulty := 1) -> void:
-	.start(difficulty)
-
