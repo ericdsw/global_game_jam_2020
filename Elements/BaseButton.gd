45bad6779b9b23210b658ba929f7311cc7839e02
William <williamromero09@gmail.com>
2020-02-02 12:42:44 -0500

    Add Sound To the Buttons

--- Elements/BaseButton.gd
+++ Elements/BaseButton.gd
@@ -2,3 +2,9 @@ extends Button
 
 func _on_BaseButton_pressed() -> void:
 	$AnimationPlayer.play("pressed")
+	$ClickButton.play()
+
+
+func _on_BaseButton_mouse_entered():
+	$ButtonHover.play()
+	pass # Replace with function body.
